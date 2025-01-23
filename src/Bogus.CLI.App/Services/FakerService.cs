c733c01d41d1c2e13335259cad8a0869fc6fc165
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2025-01-23 10:23:38 -0300

    refactor: removed variable that was not needed

--- src/Bogus.CLI.App/Services/FakerService.cs
+++ src/Bogus.CLI.App/Services/FakerService.cs
@@ -4,21 +4,17 @@ namespace Bogus.CLI.App.Services;
 public class FakerService : IFakerService
 {
     private string? _language = null;
-    private Faker? _faker = null;
     
     public DataSets.Lorem Lorem => GetFaker().Lorem;
     public DataSets.Name Name => GetFaker().Name;
     public DataSets.PhoneNumbers Phone => GetFaker().Phone;
-
+    
     public void SetLanguage(string? language)
         => _language = language;
 
-    private Faker GetFaker()
-    {
-        _faker = string.IsNullOrEmpty(_language)
-            ? new Faker() 
-            : new Faker(_language);
+    private Faker GetFaker() => string.IsNullOrEmpty(_language)
+        ? new Faker() 
+        : new Faker(_language);
 
-        return _faker!;
-    }
+
 }
